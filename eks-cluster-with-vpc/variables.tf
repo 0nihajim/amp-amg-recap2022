63ec233ce7217d856d5a94022b866a5344a766ce
0nihajim <84621389+0nihajim@users.noreply.github.com>
2023-02-11 23:08:33 +0900

    upload template

---
+++ eks-cluster-with-vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "cluster_name" {
+  description = "Name of cluster - used by Terratest for e2e test automation"
+  type        = string
+  default     = "amp-amg-recap2022"
+}
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+variable "managed_node_instance_type" {
+  description = "Instance type for the cluster managed node groups"
+  type        = string
+  default     = "t3.xlarge"
+}
+variable "managed_node_min_size" {
+  description = "Minumum number of instances in the node group"
+  type        = number
+  default     = 2
+}
+variable "eks_version" {
+  type        = string
+  description = "EKS Cluster version"
+  default     = "1.24"
+}
